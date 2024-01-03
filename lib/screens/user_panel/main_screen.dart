c7c06423bd645be4e8a2c72f3ae5a43642fb16d9
Vijay Sheoran <vijaysirsi1997@gmail.com>
2024-01-03 18:09:01 +0530

    Add app colors and keyboard management

--- lib/screens/user_panel/main_screen.dart
+++ lib/screens/user_panel/main_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shoopping_hub/utils/app_constant.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,title: Text('VIJAY'),
+        backgroundColor: AppConstant.appMainColor,
+        title: Text(
+          AppConstant.appMainName,
+          style: TextStyle(
+              fontWeight: FontWeight.w500, color: AppConstant.appTextColor),
+        ),
+        centerTitle: true,
       ),
     );
   }
